e0c16378ac8f83c814e6414880a50596e6eaa099
Sadikk <youpi.killeur@gmail.com>
2021-12-07 15:44:02 +0100

    edit eth value

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,7 +7,7 @@ const main = async () => {
   
     const Token = await hre.ethers.getContractFactory('GmPortal');
     const portal = await Token.deploy({
-      value: hre.ethers.utils.parseEther('1'),
+      value: hre.ethers.utils.parseEther('0.001'),
     });
     await portal.deployed();
   
