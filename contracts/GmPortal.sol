50afcf80d330820b9461a7f30068c8860fc803d6
Sadikk <youpi.killeur@gmail.com>
2021-12-07 16:21:22 +0100

    50% chance of winning eth

--- contracts/GmPortal.sol
+++ contracts/GmPortal.sol
@@ -6,6 +6,7 @@ import "hardhat/console.sol";
 
 contract GmPortal {
     uint256 totalGms;
+    uint256 private seed;
 
     event NewGm(address indexed from, uint256 timestamp, string nickname);
 
@@ -23,6 +24,7 @@ contract GmPortal {
 
     constructor() payable {
         console.log("Yo yo, I am a contract and I am smart");
+        seed = (block.timestamp + block.difficulty) % 100;
     }
 
     function gm(string memory _nickname) public {
@@ -31,13 +33,16 @@ contract GmPortal {
         gms.push(Gm(msg.sender, _nickname, block.timestamp));
         emit NewGm(msg.sender, block.timestamp, _nickname);
 
-        uint256 prizeAmount = 0.0001 ether;
-        require(
-            prizeAmount <= address(this).balance,
-            "Trying to withdraw more money than the contract has."
-        );
-        (bool success, ) = (msg.sender).call{value: prizeAmount}("");
-        require(success, "Failed to withdraw money from contract.");
+        if (seed <= 50) {
+            console.log("%s won!", msg.sender);
+            uint256 prizeAmount = 0.0001 ether;
+            require(
+                prizeAmount <= address(this).balance,
+                "Trying to withdraw more money than the contract has."
+            );
+            (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+            require(success, "Failed to withdraw money from contract.");
+        }
     }
 
     function getAllGms() public view returns (Gm[] memory) {
